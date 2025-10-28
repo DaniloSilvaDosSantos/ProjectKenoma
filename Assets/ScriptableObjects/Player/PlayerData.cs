52d6e2b62c146b7a24ad7211ee3eb831eadbc284
Danilo Silva dos Santos <Dsaotos4002@gmail.com>
2025-10-28 20:11:45 -0300

    Implementing The PlayerController

--- Assets/ScriptableObjects/Player/PlayerData.cs
+++ Assets/ScriptableObjects/Player/PlayerData.cs
@@ -1,13 +1,8 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerData", menuName = "Game Data/PlayerData")]
-public class PlayerData : ScriptableObject
+public class PlayerData : EntityData
 {
-    [Header("Movement Settings")]
-    public float movementSpeed = 14f;
-    public float gravity = -9.81f;
-    [Space]
-
     [Header("Camera Settings")]
     public float mouseSensitivity = 100f;
     public float minLookAngle = -90f;
@@ -15,20 +10,17 @@ public class PlayerData : ScriptableObject
     [Space]
 
     [Header("Shotgun Settings")]
-    public float shotgunAngle = 45f;
+    public float shotgunHalfAngle = 45f;
+    public float shotgunCooldown = 2.5f;
     [Space]
-    public float shotgunMaxRangeD = 12f;
     public float shotgunRangeA = 2f;
     public float shotgunRangeB = 5f;
     public float shotgunRangeC = 8f;
+    public float shotgunMaxRangeD = 12f;
     [Space]
+    
     public float shotgunMaxDamage = 10f;
     public float shotgunCloseDamage = 7f;
     public float shotgunFarDamage = 5f;
     public float shotgunVeryFarDamage = 2f;
-    [Space]
-
-    [Header("Player Stats")]
-    public float maxHealth = 100f;
-    public float startingHealth = 100f;
 }
