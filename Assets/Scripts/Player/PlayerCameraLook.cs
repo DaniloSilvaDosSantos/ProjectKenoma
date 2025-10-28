db47566a2a981d3dbb40c6e003ace9fefdf893f0
Danilo Silva dos Santos <Dsaotos4002@gmail.com>
2025-10-28 20:13:02 -0300

    Implementing Player Camera Movement and Rotation

---
+++ Assets/Scripts/Player/PlayerCameraLook.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerCameraLook : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private PlayerController controller;
+    [SerializeField] private Transform playerBody;
+
+    [Header("Variables")]
+    [SerializeField] private float xRotation = 0f;
+
+    private void Start()
+    {
+        controller = GetComponentInParent<PlayerController>();
+
+        if (controller == null)
+        {
+            Debug.LogError("PlayerController don't have been found in a parent object!");
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (!controller.isAlive) return;
+
+        float mouseSensitivity = controller.playerData.mouseSensitivity;
+        float minYAngle = controller.playerData.minLookAngle;
+        float maxYAngle = controller.playerData.maxLookAngle;
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, minYAngle, maxYAngle);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
+
