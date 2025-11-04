efc37c55370b74ef7cbaa06430fae21bd4a8f96a
Danilo Silva dos Santos <Dsaotos4002@gmail.com>
2025-11-04 20:43:18 -0300

    Implementing A Maximum Number Of Instatiates Enemies And A Enemy Queue For Spawn

--- Assets/ScriptableObjects/Waves/WavesRound/WavesDatabase.cs
+++ Assets/ScriptableObjects/Waves/WavesRound/WavesDatabase.cs
@@ -3,5 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Game Data/Waves Database")]
 public class WavesDatabase : ScriptableObject
 {
+    [Header("Spawn Variables")]
+    public float minSpawnDist = 30f;
+    public float maxSpawnDist = 120f;
+    public int maxAliveEnemies = 80;
+    [Space]
+
+    [Header("All Waves")]
     public WaveData[] waves;
 }
