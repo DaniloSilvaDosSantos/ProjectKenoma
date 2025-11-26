135c34d972cff1089cc97338366dcb17a89d8af0
Danilo Silva dos Santos <Dsaotos4002@gmail.com>
2025-11-25 22:30:10 -0300

    Implementing The Player Shotgun Shot And Reloud SFX

--- Assets/Scripts/AnimationEventProxy.cs
+++ Assets/Scripts/AnimationEventProxy.cs
@@ -5,8 +5,19 @@ public class AnimationEventProxy : MonoBehaviour
     [Header("References")]
     [SerializeField] private AudioSource audioSource;
 
+    // PLAYER //
     public void PlayFootstep()
     {
         Radio.Instance.PlaySFX("SFX/PlayerWalking", audioSource);
+    }
+
+    public void PlayShotgunShot()
+    {
+        Radio.Instance.PlaySFX("SFX/ShotgunShot", audioSource);
+    }
+
+    public void PlayShotgunReload()
+    {
+        Radio.Instance.PlaySFX("SFX/ShotgunReload", audioSource);
     }
 }
