8f770cd60d8edbe57b5dd6c18b1b2429e0560e80
Danilo Silva dos Santos <Dsaotos4002@gmail.com>
2025-12-09 20:27:19 -0300

    Implementing The GameController

---
+++ Assets/GameController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController gameController;
+    [SerializeField] private Image FadeScreen;
+
+    public string currentScene = "";
+    public string sceneToChange = "";
+
+    public bool Fade = false;
+    public int points = 0;
+
+    void Awake()
+    {
+        gameController = this;
+        //ChangeScene(sceneToChange);
+    }
+
+    void Update()
+    {
+        HandleFade();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (currentScene != "MainMenu")
+            {
+                ChangeScene("MainMenu");
+            }
+        }
+    }
+
+    public void OffFade()
+    {
+        Fade = false;
+    }
+
+    void HandleFade()
+    {
+        var tempColor = FadeScreen.color;
+
+        if (tempColor.a < 0.05f) FadeScreen.enabled = false;
+        else FadeScreen.enabled = true;
+
+        if (Fade && tempColor.a < 1f)
+        {
+            tempColor.a = Mathf.Min(tempColor.a + 1f * Time.deltaTime, 1f);
+            FadeScreen.color = tempColor;
+        }
+        else if (!Fade && tempColor.a > 0f)
+        {
+            tempColor.a = Mathf.Max(tempColor.a - 1f * Time.deltaTime, 0f);
+            FadeScreen.color = tempColor;
+        }
+    }
+
+    public void ChangeScene(string newScene, bool doFade = true)
+    {
+        if (doFade)Fade = true;
+
+        sceneToChange = newScene;
+
+        Invoke("DoTheSceneChange", 2f);
+    }
+
+    public void DoTheSceneChange()
+    {
+        SceneManager.LoadScene(sceneToChange, LoadSceneMode.Single);
+        currentScene = sceneToChange;
+
+        Invoke("OffFade", 2f);
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
